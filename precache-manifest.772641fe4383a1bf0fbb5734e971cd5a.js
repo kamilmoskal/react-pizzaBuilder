8d11e4a45d5ef3b273d5f517e14b563287bab79e
kamilmoskal <kamil_moskal@interia.pl>
2019-03-25 12:23:16 +0100

    Updates

---
+++ precache-manifest.772641fe4383a1bf0fbb5734e971cd5a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4124e2bb779f2d35e99",
+    "url": "/react-pizzaBuilder/static/js/8.61433c7a.chunk.js"
+  },
+  {
+    "revision": "eea93c4f591f9b1afb6b",
+    "url": "/react-pizzaBuilder/static/js/0.9c7b7d2c.chunk.js"
+  },
+  {
+    "revision": "f7344463c880b6a41d08005fbce47cf0",
+    "url": "/react-pizzaBuilder/static/media/salami.f7344463.svg"
+  },
+  {
+    "revision": "415fd193722d87686a5b",
+    "url": "/react-pizzaBuilder/static/js/main.7c956ff7.chunk.js"
+  },
+  {
+    "revision": "705e4c07ffe9274fd813",
+    "url": "/react-pizzaBuilder/static/js/runtime~main.91d397c0.js"
+  },
+  {
+    "revision": "56445df915d0a8c54237",
+    "url": "/react-pizzaBuilder/static/js/4.d44e467c.chunk.js"
+  },
+  {
+    "revision": "04faaea757225c738bf2de28db1c8838",
+    "url": "/react-pizzaBuilder/static/media/redPepper.04faaea7.svg"
+  },
+  {
+    "revision": "38ed1b2436380ab543c5",
+    "url": "/react-pizzaBuilder/static/js/5.b76a315b.chunk.js"
+  },
+  {
+    "revision": "0d1687bc446961892f59319a7fd8f3fa",
+    "url": "/react-pizzaBuilder/static/media/pineapple.0d1687bc.svg"
+  },
+  {
+    "revision": "3d332f8567c39dd0f042",
+    "url": "/react-pizzaBuilder/static/js/6.1b4b5273.chunk.js"
+  },
+  {
+    "revision": "df14f2c72864feb1af747428fb77bb31",
+    "url": "/react-pizzaBuilder/static/media/onion.df14f2c7.svg"
+  },
+  {
+    "revision": "7693cbbf96cbdf7741ea",
+    "url": "/react-pizzaBuilder/static/js/7.000f575a.chunk.js"
+  },
+  {
+    "revision": "466ae5e9c84fee0e403facff7930284a",
+    "url": "/react-pizzaBuilder/static/media/olives.466ae5e9.svg"
+  },
+  {
+    "revision": "79ba09449ffe06ff466f",
+    "url": "/react-pizzaBuilder/static/js/1.ef069d71.chunk.js"
+  },
+  {
+    "revision": "f3dede4e84892b9084a6",
+    "url": "/react-pizzaBuilder/static/js/9.0bd84ce7.chunk.js"
+  },
+  {
+    "revision": "96deab5166d171f2795881612e12aea2",
+    "url": "/react-pizzaBuilder/static/media/basic.96deab51.svg"
+  },
+  {
+    "revision": "186aab84059b497954227a8113bf6da1",
+    "url": "/react-pizzaBuilder/static/media/basil.186aab84.svg"
+  },
+  {
+    "revision": "f2f89bc38056e20b9000908e7a6bbed3",
+    "url": "/react-pizzaBuilder/static/media/dough.f2f89bc3.svg"
+  },
+  {
+    "revision": "6148065362a49512ea9d40ccde162eef",
+    "url": "/react-pizzaBuilder/static/media/greenPepper.61480653.svg"
+  },
+  {
+    "revision": "a60c76e0d3e8b8a5cfdb59471bd8534e",
+    "url": "/react-pizzaBuilder/static/media/mushrooms.a60c76e0.svg"
+  },
+  {
+    "revision": "415fd193722d87686a5b",
+    "url": "/react-pizzaBuilder/static/css/main.5c6bcdf7.chunk.css"
+  },
+  {
+    "revision": "f4124e2bb779f2d35e99",
+    "url": "/react-pizzaBuilder/static/css/8.cd8dcb02.chunk.css"
+  },
+  {
+    "revision": "7693cbbf96cbdf7741ea",
+    "url": "/react-pizzaBuilder/static/css/7.0ab40ff6.chunk.css"
+  },
+  {
+    "revision": "3d332f8567c39dd0f042",
+    "url": "/react-pizzaBuilder/static/css/6.f9b5c628.chunk.css"
+  },
+  {
+    "revision": "38ed1b2436380ab543c5",
+    "url": "/react-pizzaBuilder/static/css/5.fed6d775.chunk.css"
+  },
+  {
+    "revision": "0fbb86eddb0c7d05aa4f3590460584e1",
+    "url": "/react-pizzaBuilder/index.html"
+  }
+];
